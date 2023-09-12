1a42a2370c70caa9d4799d4707d5484804843482
ks209 <ks2092003@gmail.com>
2023-09-12 17:35:00 +0530

    Final 2

--- script.js
+++ script.js
@@ -2,24 +2,47 @@ const colorPicker = document.getElementById("colorPicker");
         const pixels = document.querySelectorAll(".pixel");
         var selectedColor = "black";
         var isMouseDown = false;
+        var isTouchDown = false;
 
         colorPicker.addEventListener("input", updateColor);
 
         pixels.forEach((pixel) => {
             pixel.addEventListener('mousedown', () => {
+                event.preventDefault();
                 isMouseDown = true;
                 pixel.style.backgroundColor = selectedColor;
+                console.log("mousedown");
             });
 
             pixel.addEventListener('mouseover', () => {
                 if (isMouseDown) {
                     pixel.style.backgroundColor = selectedColor;
                 }
+                console.log("mousemove");
             });
 
             pixel.addEventListener('mouseup', () => {
                 isMouseDown = false;
+                console.log("mouseup");
             });
+
+            pixel.addEventListener('touchstart', (event) => {
+                event.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
+                isTouchDown = true;
+                pixel.style.backgroundColor = selectedColor;
+            });
+        
+            pixel.addEventListener('touchmove', (event) => {
+                event.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
+                if (isTouchDown) {
+                    pixel.style.backgroundColor = selectedColor;
+                }
+            });
+        
+            pixel.addEventListener('touchend', () => {
+                isTouchDown = false;
+            });
+
         });
 
         function updateColor(event) {
@@ -32,3 +55,5 @@ const colorPicker = document.getElementById("colorPicker");
         var clear=document.getElementById("clear")
 
         clear.addEventListener("click",function(){location.reload()})
+
+        console.log("connected")
